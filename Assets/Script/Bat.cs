da942d159ff1e1a9e835523348aa3a356c984c94
AfganisthanKing <46166566+AfganisthanKing@users.noreply.github.com>
2019-10-04 10:34:55 +0530

    update 2

---
+++ Assets/Script/Bat.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bat : MonoBehaviour
+{
+    [SerializeField]
+    private BatCubeFollowers _BatCubeFollowers;
+
+
+    //spawn bat inside cube for physics
+    private void SpawnBatCubeFollower()
+    {
+        var follower = Instantiate(_BatCubeFollowers);
+        follower.transform.position = transform.position;
+        follower.SetFollowTarget(this);
+        
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnBatCubeFollower();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       // Vector3 _vel = OVRInput.GetLocalControllerVelocity(OVRInput.Controller.RTouch);
+       // Debug.Log(_vel);
+    }
+    public void OnCollisionEnter(Collision collision)
+    {
+        //if (collision.gameObject.CompareTag("Ball"))
+        //{
+        //    OVRHaptics.Channels[1].Preempt(new OVRHapticsClip());
+        //}
+    }
+}
---
+++ Assets/Script/BatCubeFollowers.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BatCubeFollowers : MonoBehaviour
+{
+    private Bat _batFollower;
+    private Rigidbody _rigidbody;
+    private Vector3 _velocity; //calculate the velocity user produce
+
+    [SerializeField]
+    private float _sensitivity = 100f;
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+        _batFollower = GetComponentInParent<Bat>();
+    }
+
+    private void FixedUpdate()
+    {
+        //calculating the velocity of controller user produced and applay to rigidbody
+        Vector3 destination = _batFollower.transform.position;
+        _rigidbody.transform.rotation = transform.rotation;
+
+        _velocity = (destination - _rigidbody.transform.position) * _sensitivity;
+
+        _rigidbody.velocity = _velocity;
+        transform.rotation = _batFollower.transform.rotation;
+    }
+
+    internal void SetFollowTarget(Bat bat)
+    {
+        _batFollower = bat;
+    }
+    public void OnCollisionEnter(Collision collision)
+    {
+        //if (collision.gameObject.CompareTag("Ball"))
+        //{
+        //    OVRHaptics.Channels[1].Preempt(new OVRHapticsClip());
+        //}
+    }
+}
